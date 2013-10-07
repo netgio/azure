f855a861ee5020e538e1de35730d8ee64f326266
netgio <gary@netgio.com>
2013-10-07 17:12:21 +0100

    fixed bug (passing lat twice)

--- api/weather.js
+++ api/weather.js
@@ -1,7 +1,7 @@
 exports.get = function(request, response) {
 
     if (request.query.lat != null && request.query.lon != null) {
-        performRequest('/forecast', 'GET', request.query.lat , request.query.lat, function(data) {
+        performRequest('/forecast', 'GET', request.query.lat , request.query.lon, function(data) {
             response.send(statusCodes.OK, { message : data} );
         });
     }
